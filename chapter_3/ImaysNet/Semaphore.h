6c8d19c8c3bbc0667b8655758b6d84487915c04d
jeongminje <jjmyann@gmail.com>
2019-04-27 12:07:49 +0900

    vs 2017 build on windows 7
    windows 7 에서는 8.1 sdk 로 빌드해도 깔끔하게 빌드되지는 않음.

--- chapter_3/ImaysNet/Semaphore.h
+++ chapter_3/ImaysNet/Semaphore.h
@@ -5,6 +5,13 @@
 #include <mutex>
 #include <condition_variable>
 
+/*
+	windows 환경에 국한된다면 system api 로 semaphore 를 지원하니까 직접 만들 필요가 없지만,
+	크로스 플랫폼 환경이면 아래 구현이 유용할 듯 싶다.
+
+	condition_variable. event 대신 쓰기 좋은 자료형이네.
+	https://m.blog.naver.com/jidon333/220182028669
+*/
 class Semaphore {
 public:
 	Semaphore(int count_ = 0)
